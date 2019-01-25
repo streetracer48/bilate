088808ada027ba50b650952085d22b3bc6aadfaa
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-25 12:03:37 +0600

    navbar desgin done

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -1,36 +1,47 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import logo from '../logo.svg'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../logo.svg";
+import { ButtonContainer } from "./button";
 export default class Navbar extends Component {
   render() {
     return (
-
-        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
-
+      <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        {/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */}
         <Link to="/">
-        <img src={logo} alt="store" className="navbar-brand" />
+          <img src={logo} alt="store" className="navbar-brand" />
         </Link>
-
-         <ul className="navbar-nav align-items-center">
-           <li className="nav-item ml-5">          
-           <Link to="/" className="nav-link">
-             Products
-             </Link>
-
-           </li>          
-           
-         </ul>
-
-         <Link to="/cart" className="ml-auto">
-         <button>
-             <i className="fas fa-cart-plus" />
-
-        </button>
-         </Link>
-
-        </nav>
-      
-    )
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus " />
+            </span>
+            my cart
+          </ButtonContainer>
+        </Link>
+      </Nav>
+    );
   }
 }
+
+const Nav = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+  @media (max-width: 576px) {
+    .navbar-nav {
+      flex-direction: row !important;
+`;
