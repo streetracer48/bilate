911c7c7ce6d1204d1f9c08ef2343a3b6bdf0da58
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-25 12:16:50 +0600

    showing products title as props

--- src/Component/productList.jsx
+++ src/Component/productList.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Product from './product'
+import Title from './title'
 
 export default class ProductList extends Component {
   render() {
     return (
-      <div>
-        Hello Product
-      </div>
+      <React.Fragment>
+         <productWrapper className="py-5">
+         <div className="container">
+         <Title name="our" title="Products"/>  
+         </div>
+          </productWrapper>
+      </React.Fragment>
     )
   }
 }
